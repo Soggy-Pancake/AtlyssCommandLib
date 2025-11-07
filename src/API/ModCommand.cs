67b6e7622e3c26c08638827f1ec8cdaeec5ca843
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2025-11-07 14:39:16 -0800

    Use unbracketed namespace

--- src/API/ModCommand.cs
+++ src/API/ModCommand.cs
@@ -3,53 +3,52 @@ using System.Collections.Generic;
 using System.Text;
 using static AtlyssCommandLib.API.Utils;
 
-namespace AtlyssCommandLib.API {
+namespace AtlyssCommandLib.API; 
+
+/// <summary>
+/// A Command object that contains the callback and help message for a command.
+/// </summary>
+public class ModCommand {
+
+    internal string Command;
+    string HelpMessage;
+
+    internal CommandCallback Callback;
+    internal bool clientSideCommand;
+    internal bool serverSideCommand;
+    internal bool consoleCommand;
 
     /// <summary>
-    /// A Command object that contains the callback and help message for a command.
+    /// Creates a new ModCommand.
     /// </summary>
-    public class ModCommand {
+    /// <param name="command"></param>
+    /// <param name="helpMessage"></param>
+    /// <param name="callback"></param>
+    /// <param name="clientSide"></param>
+    /// <param name="serverSide"></param>
+    /// <param name="console"></param>
+    public ModCommand(string command, string helpMessage, CommandCallback callback, bool clientSide, bool serverSide, bool console) {
+        Command = command;
+        HelpMessage = helpMessage;
+        Callback = callback;
+        clientSideCommand = clientSide;
+        serverSideCommand = serverSide;
+        consoleCommand = console;
+    }
+    /// <summary>
+    /// Send help message to caller.
+    /// </summary>
+    /// <param name="caller"></param>
+    public void printHelp(Caller caller) {
+        string message = $"/{Command} - {HelpMessage}";
+        NotifyCaller(caller, message);
+    }
 
-        internal string Command;
-        string HelpMessage;
-
-        internal CommandCallback Callback;
-        internal bool clientSideCommand;
-        internal bool serverSideCommand;
-        internal bool consoleCommand;
-
-        /// <summary>
-        /// Creates a new ModCommand.
-        /// </summary>
-        /// <param name="command"></param>
-        /// <param name="helpMessage"></param>
-        /// <param name="callback"></param>
-        /// <param name="clientSide"></param>
-        /// <param name="serverSide"></param>
-        /// <param name="console"></param>
-        public ModCommand(string command, string helpMessage, CommandCallback callback, bool clientSide, bool serverSide, bool console) {
-            Command = command;
-            HelpMessage = helpMessage;
-            Callback = callback;
-            clientSideCommand = clientSide;
-            serverSideCommand = serverSide;
-            consoleCommand = console;
-        }
-        /// <summary>
-        /// Send help message to caller.
-        /// </summary>
-        /// <param name="caller"></param>
-        public void printHelp(Caller caller) {
-            string message = $"/{Command} - {HelpMessage}";
-            NotifyCaller(caller, message);
-        }
-
-        /// <summary>
-        /// Get the help message for this command.
-        /// </summary>
-        /// <returns></returns>
-        public string getHelpMessage() { 
-            return HelpMessage;
-        }
+    /// <summary>
+    /// Get the help message for this command.
+    /// </summary>
+    /// <returns></returns>
+    public string getHelpMessage() { 
+        return HelpMessage;
     }
 }
--- src/Packets.cs
+++ src/Packets.cs
@@ -5,64 +5,64 @@ using System.Text;
 using CodeTalker.Networking;
 using CodeTalker.Packets;
 
-namespace AtlyssCommandLib {
-    internal class ServerCommandPkt : BinaryPacketBase {
+namespace AtlyssCommandLib;
 
-        public override string PacketSignature => "ACL_ServerCmds";
-        public List<Entry> entries = new List<Entry>();
+internal class ServerCommandPkt : BinaryPacketBase {
 
-        internal struct Entry {
-            public string prefix;
-            public string helpMessage;
+    public override string PacketSignature => "ACL_ServerCmds";
+    public List<Entry> entries = new List<Entry>();
+
+    internal struct Entry {
+        public string prefix;
+        public string helpMessage;
+    }
+
+    public ServerCommandPkt() { }
+
+    public override byte[] Serialize() {
+        CommandManager.updateServerCommands();
+
+        var ms = new MemoryStream();
+        var buffer = new BinaryWriter(ms);
+
+        buffer.Write((byte)CommandManager.serverCommands.Count);
+        foreach (var cmd in CommandManager.serverCommands) {
+            buffer.Write(Encoding.UTF8.GetBytes(cmd.Key + '\0'));
+            buffer.Write(Encoding.UTF8.GetBytes(cmd.Value + '\0'));
         }
 
-        public ServerCommandPkt() { }
+        Plugin.logger?.LogMessage($"Full servercmd hex: {BitConverter.ToString(ms.ToArray()).Replace("-", "")}");
+        return ms.ToArray();
+    }
 
-        public override byte[] Serialize() {
-            CommandManager.updateServerCommands();
+    public override void Deserialize(byte[] data) {
 
-            var ms = new MemoryStream();
-            var buffer = new BinaryWriter(ms);
+        var ms = new MemoryStream(data);
+        var buffer = new BinaryReader(ms);
 
-            buffer.Write((byte)CommandManager.serverCommands.Count);
-            foreach (var cmd in CommandManager.serverCommands) {
-                buffer.Write(Encoding.UTF8.GetBytes(cmd.Key + '\0'));
-                buffer.Write(Encoding.UTF8.GetBytes(cmd.Value + '\0'));
-            }
+        byte count = buffer.ReadByte();
+        short start = 1, end1 = 0, end2 = 0;
+        while (ms.Position < ms.Length) {
+            if (buffer.Read() == 0) {
+                if (end1 == 0) {
+                    end1 = (short)ms.Position;
+                } else if (end2 == 0) {
+                    end2 = (short)ms.Position;
 
-            Plugin.logger?.LogMessage($"Full servercmd hex: {BitConverter.ToString(ms.ToArray()).Replace("-", "")}");
-            return ms.ToArray();
-        }
+                    string prefix = Encoding.UTF8.GetString(data, start, end1 - start - 1);
+                    string helpMessage = Encoding.UTF8.GetString(data, end1, end2 - end1 - 1);
 
-        public override void Deserialize(byte[] data) {
+                    entries.Add(new Entry { prefix = prefix, helpMessage = helpMessage });
 
-            var ms = new MemoryStream(data);
-            var buffer = new BinaryReader(ms);
-
-            byte count = buffer.ReadByte();
-            short start = 1, end1 = 0, end2 = 0;
-            while (ms.Position < ms.Length) {
-                if (buffer.Read() == 0) {
-                    if (end1 == 0) {
-                        end1 = (short)ms.Position;
-                    } else if (end2 == 0) {
-                        end2 = (short)ms.Position;
-
-                        string prefix = Encoding.UTF8.GetString(data, start, end1 - start - 1);
-                        string helpMessage = Encoding.UTF8.GetString(data, end1, end2 - end1 - 1);
-
-                        entries.Add(new Entry { prefix = prefix, helpMessage = helpMessage });
-
-                        end1 = 0;
-                        end2 = 0;
-                        start = (short)ms.Position;
-                    }
+                    end1 = 0;
+                    end2 = 0;
+                    start = (short)ms.Position;
                 }
             }
+        }
 
-            if(entries.Count != count) {
-                Plugin.logger?.LogWarning($"Expected {count} server commands but got {entries.Count}!");
-            }
+        if(entries.Count != count) {
+            Plugin.logger?.LogWarning($"Expected {count} server commands but got {entries.Count}!");
         }
     }
 }
