112d3a8090a646258c79476600e76de8b7f012c9
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2026-01-02 08:09:01 -0700

    Update expected game version

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -7,7 +7,7 @@ internal static class PluginInfo {
 
     internal const string GUID = "Soggy_Pancake.CommandLib";
     internal const string NAME = "AtlyssCommandLib";
-    internal const string GAMEVER = "112025.a3";
+    internal const string GAMEVER = "112025.a4";
 
     // NO TOUCHIE | PUT THE NEW VERSION NUMBER IN THE .csproj
     internal const string VERSION = "0.0.3";
