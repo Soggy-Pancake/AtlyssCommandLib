e13ddffdf01bb1df8d1274ada49c9ad6065369b6
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2026-01-10 12:03:39 -0700

    Fix broken easysettings shenanagins

--- src/Config.cs
+++ src/Config.cs
@@ -6,20 +6,23 @@ using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using Nessie.ATLYSS.EasySettings;
 
-namespace AtlyssCommandLib; 
+namespace AtlyssCommandLib;
 
 internal class ModConfig {
 
+    public static ConfigFile? configFile;
     public static ConfigEntry<bool>? enableListingMods;
     public static ConfigEntry<bool>? sendFailedCommands; // Allows sending commands that failed to parse client side to server anyway
 
     public static void init(ConfigFile config) {
 
+        configFile = config;
         enableListingMods = config.Bind("General", "EnableModListing", true, "Enable the /mods command to list server mods.");
-        sendFailedCommands = config.Bind("General", "SendFailedCommandsToServer", false, "If a command fails to parse on the client side, still send it to the server. Useful for commands that only run server side. CommandLib will no longer print the error help message! The server will still block commands from properly entering chat, but if the server is vanilla it will enter chat!");
+        sendFailedCommands = config.Bind("General", "SendFailedCommandsToServer", true, "If a command fails to parse on the client side, still send it to the server. Useful for commands that only run server side. CommandLib will no longer print the error help message! The server will still block commands from properly entering chat, but if the server is vanilla it will enter chat!");
 
         if (Chainloader.PluginInfos.ContainsKey("EasySettings")) {
             try {
+                Plugin.logger?.LogInfo("EasySettings detected, adding settings tab.");
                 easySettings();
             } catch { }
         }
@@ -27,12 +30,15 @@ internal class ModConfig {
     }
 
     [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void easySettings() { 
-        SettingsTab tab = Settings.ModTab;
+    public static void easySettings() {
+        Settings.OnApplySettings.AddListener(() => { configFile?.Save(); });
 
-        tab.AddHeader("Atlyss Command Lib Settings");
-        tab.AddToggle(enableListingMods);
-        tab.AddToggle(sendFailedCommands);
+        Settings.OnInitialized.AddListener(() => {
+            SettingsTab tab = Settings.ModTab;
+
+            tab.AddHeader("Atlyss Command Lib Settings");
+            tab.AddToggle(enableListingMods);
+            tab.AddToggle(sendFailedCommands);
+        });
     }
-
 }
